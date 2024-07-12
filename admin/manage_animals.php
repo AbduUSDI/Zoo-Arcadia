e33bedff60865d350277c5b4f1c2846804f1d713
Abdurahman Usdi <Karausdi77@outlook.fr>
2024-07-12 21:50:34 +0200

    ok comiit

--- admin/manage_animals.php
+++ admin/manage_animals.php
@@ -12,16 +12,15 @@ require '../functions.php';
 
 // Connexion à la base de données
 
-$db = new Database();
-$conn = $db->connect();
+$db = (new Database())->connect();
 
-// Instance pour utililser les méthodes en rapport avec les habitats
+// Instance Animal pour utiliser les méthodes préparées en rapport avec les animaux
 
-$habitat = new Habitat($conn);
+$animal = new Animal($db);
 
-// Utilisation de la méthode "getToutHabitats" pour afficher tout les habitats sur un tableau
+// Utilisation de la méthode "getAll" pour récupérer les infos de tout les animaux
 
-$habitats = $habitat->getToutHabitats();
+$animals = $animal->getAll();
 
 include '../templates/header.php';
 include 'navbar_admin.php';
@@ -40,39 +39,44 @@ body {
     border-radius: 15px;
 }
 </style>
+<!-- Conteneur pour afficher le tableau de gestion des animaux -->
 
 <div class="container mt-4">
-    <h1 class="my-4">Gestion des Habitats</h1>
-    <a href="add_habitat.php" class="btn btn-success mb-4">Ajouter un habitat</a>
-<div class="table-responsive">
-                <table class="table table-bordered table-striped table-hover">
-                    <thead class="thead-dark">
-            <tr>
-                <th>ID</th>
-                <th>Nom</th>
-                <th>Image</th>
-                <th>Description</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($habitats as $habitat) : ?>
+    <h1 class="my-4">Gestion des Animaux</h1>
+    <div class="table-responsive">
+
+    <!-- Bouton pour ajouter un animal -->
+
+        <a href="add_animal.php" class="btn btn-success mb-4">Ajouter un Animal</a>
+        <table class="table table-bordered table-striped table-hover">
+            <thead class="thead-dark">
                 <tr>
-                    <td><?php echo htmlspecialchars($habitat['id']); ?></td>
-                    <td><?php echo htmlspecialchars($habitat['name']); ?></td>
-                    <td><?php if (!empty($habitat['image'])): ?>
-                            <img src="../uploads/<?php echo htmlspecialchars($habitat['image']); ?>" alt="Image de l'habitat" style="width: 100px;">
-                        <?php endif; ?>
-                    </td>
-                    <td><?php echo htmlspecialchars($habitat['description']); ?></td>
-                    <td>
-                        <a href="edit_habitat.php?id=<?php echo $habitat['id']; ?>" class="btn btn-warning">Modifier</a>
-                        <a href="delete_habitat.php?id=<?php echo $habitat['id']; ?>" class="btn btn-danger" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet habitat ?');">Supprimer</a>
-                    </td>
+                    <th>ID</th>
+                    <th>Prénom</th>
+                    <th>Race</th>
+                    <th>Image</th>
+                    <th>Actions</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($animals as $animal): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($animal['id']); ?></td>
+                        <td><?php echo htmlspecialchars($animal['name']); ?></td>
+                        <td><?php echo htmlspecialchars($animal['species']); ?></td>
+                        <td><img src="../uploads/<?php echo htmlspecialchars($animal['image']); ?>" alt="<?php echo htmlspecialchars($animal['name']); ?>" width="100"></td>
+                        <td>
+
+                            <!-- Boutons pour modifier ou supprimer (Message de confirmation si il faut supprimer ou pas) un animal -->
+
+                            <a href="edit_animal.php?id=<?php echo htmlspecialchars($animal['id']); ?>" class="btn btn-warning">Modifier</a>
+                            <a href="delete_animal.php?id=<?php echo htmlspecialchars($animal['id']); ?>" class="btn btn-danger" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet animal ?');">Supprimer</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </div>
-</div>
+
 <?php include '../templates/footerconnected.php'; ?>
